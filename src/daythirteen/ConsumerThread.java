841fd4e5ff2684f6213d3cbf67ca4854af5b1666
Python <sagarrothe902@gmail.com>
2024-06-12 00:09:30 +0530

    13th commit

---
+++ src/daythirteen/ConsumerThread.java
@@ -0,0 +1,22 @@
+package daythirteen;
+
+public class ConsumerThread extends Thread{
+	
+	Product p;
+
+	public ConsumerThread(Product p) {
+		super();
+		this.p = p;
+	}
+	
+	public void run() {
+		while(true) {
+			try {
+				p.get();
+			} catch (InterruptedException e) {
+				
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/daythirteen/ProducerThread.java
@@ -0,0 +1,21 @@
+package daythirteen;
+
+public class ProducerThread extends Thread{
+	Product p;
+
+	public ProducerThread(Product p) {
+		super();
+		this.p = p;
+	}
+	
+	public void run() {
+		while(true) {
+			try {
+				p.put();
+			} catch (InterruptedException e) {
+			
+				e.printStackTrace();
+			}
+		}
+	}
+}
