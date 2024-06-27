75823b85611dbed64a684ca83d79d16259e37872
Python <sagarrothe902@gmail.com>
2024-06-27 16:04:05 +0530

    22th commit

--- src/testcases/businesslogic/Operations.java
+++ src/testcases/businesslogic/Operations.java
@@ -6,4 +6,40 @@ public class Operations {
 		
 		return a+b;
 	}
+	
+	public static long getFactorial(int n) {
+		int f=1;
+		if(n<0) {
+			return -1;
+		}
+		for(int i=1;i<=n;i++) {
+			f=f*i;
+			
+		}
+		return f;
+	}
+	
+	public static boolean isPrime(int n) {
+		int n1=n;
+		for(int i=2;i<n;i++) {
+			if(n1<0 || n%i==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static boolean isPalindrome(int n) {
+		
+		int n1 = n, rev = 0;
+		while(n!=0) {
+			
+			rev= rev*10 + n%10;
+			n = n/10;
+		}
+		
+		return n1 == rev;
+	}
+	
+	
 }
--- src/testcases/testing/MyTestClass.java
+++ src/testcases/testing/MyTestClass.java
@@ -9,11 +9,62 @@ public class MyTestClass {
 
 	@Test
 	public void testAddition() {
+		System.out.println("Addition Testing");
 		
 		int expected = 7;
 		int actual = Operations.add(2, 5);
 		
 		Assertions.assertEquals(expected, actual);
 		
+	}
+	
+	@Test
+	public void testFactorial() {
+		System.out.println("Factorial Testing");
+		
+		long expected = 120;
+		long actual = Operations.getFactorial(5);
+		
+		Assertions.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testFactorialForZero() {
+		System.out.println("Factorial Testing");
+		
+		long expected = 1;
+		long actual = Operations.getFactorial(0);
+		
+		Assertions.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testFactorialForNegativeNumbers() {
+		System.out.println("Factorial Testing");
+		
+		long expected = -1;
+		long actual = Operations.getFactorial(-2);
+		
+		Assertions.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void isPrime() {
+		System.out.println("Prime Number Testing");
+		Assertions.assertTrue(Operations.isPrime(3));
+	}
+	
+	@Test
+	public void testForNonPrime() {
+		
+		System.out.println("Non Prime testing");
+		
+		Assertions.assertFalse(Operations.isPrime(4));
+	}
+	
+	@Test
+	public void isPalindrome() {
+		System.out.println("Palindrome Testing");
+		Assertions.assertTrue(Operations.isPalindrome(121));
 	}
 }
